cf16375d3800d4e65f6b9aeb473918009d61d403
Augusto <augusto_conceicao@hotmail.com>
2020-10-30 10:09:33 -0300

    Ajustes CORs e adição das imagens.

--- Projeto/shopping-cerveja-back/src/main/java/br/com/delis/ShoppingCerveja/CorsConfig.java
+++ Projeto/shopping-cerveja-back/src/main/java/br/com/delis/ShoppingCerveja/CorsConfig.java
@@ -1,8 +1,8 @@
 package br.com.delis.ShoppingCerveja;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.web.servlet.config.annotation.CorsRegistry;
+        import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer
